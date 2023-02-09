0770152302dc9856b741c60cba9ab9cde7d5cb52
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-09 18:14:17 +0300

    Project 3. Summary of digits for number

---
+++ Homework/Project3_SumOfNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿//35. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+Console.WriteLine("Привет! Данная программа считает сумму чисел через метод С#");
+Start:
+Console.WriteLine("Введите целое число: ");
+
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{Sum(inputNumber)}");
+
+int Sum (int x)
+{
+   int i, sum=0;
+   while (x != 0)
+   {
+      i = x % 10;
+      sum=sum + i;
+      x /= 10;
+   }
+   sum = sum + x;
+   return sum;
+}
+
+goto Start;
