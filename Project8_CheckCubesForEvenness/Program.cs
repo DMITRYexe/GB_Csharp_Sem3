cedd4d2c04e99d7548286f1edfcece7d77b944bb
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-09 08:12:09 +0300

    Project 8. Check cubes of numbers for the evennes

---
+++ Project8_CheckCubesForEvenness/Program.cs
@@ -0,0 +1,24 @@
+﻿// Проверка кубов чисел на четность. Использовать подпрограммы
+
+bool Test (int n)
+{
+   if (n % 2 == 0) 
+   {
+      return true;
+   }
+   else 
+   {
+      return false;
+   }
+}
+
+Random random=new Random();
+for(int i = 0; i<10; i++)
+{
+   int a=random.Next(1,100);
+   int b = (int)Math.Pow(a, 3);
+   if (Test(b))
+   {
+      Console.WriteLine(b);
+   }
+}
