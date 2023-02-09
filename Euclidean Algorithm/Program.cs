c24027cd0c2d56a6e8eefa6e6f7ccb5da47659c6
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-09 07:14:43 +0300

    Project 6. Euclidean algorithm

--- Euclidean Algorithm/Program.cs
+++ Euclidean Algorithm/Program.cs
@@ -1,11 +1,13 @@
 ﻿// Алгоритм Евклида. (Нахождение НОД).
 void NOD(int a=11, int b=121)
 {
+   int a1 =a; int b1 = b;
    while (a != b)
       if (a > b)
          a -= b; // сокр. запись a=a-b
       else
          b -= a;
-   System.Console.WriteLine(a);
+   System.Console.WriteLine(a); // (НОД)
+   System.Console.WriteLine(a1*b1/a); // Наименьшее общ. кратное (НОК)
 }
 NOD();
