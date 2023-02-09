74f546d6d0eb205184773edfaaffc7cc6d9f0db1
Дмитрий Коротчук <dmitrykorotchuk@mail.ru>
2023-02-09 07:52:36 +0300

    Project 7. Digits Counter

---
+++ Project7_DigitsCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿//34. Определить количество цифр в числе. Сделать подпрограмму.
+using System.Diagnostics.Metrics;
+
+int DigitsCounter (int n)
+{
+   int counter = 0;
+   if (n!=0)
+   {
+      while (n!=0) 
+      { counter++; n/=10; }
+      return counter;
+   }
+   else
+   {
+      return 1;
+   }
+}
+
+Console.WriteLine (DigitsCounter(59));
